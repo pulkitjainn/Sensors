75873e3da199780fb74dc1d3e319943f98413c83
pulkitjainn <pulkitjain610@gmail.com>
2017-04-25 18:02:37 +0530

    Sensors

--- app/src/main/java/com/example/pulkit/sensors/MainActivity.java
+++ app/src/main/java/com/example/pulkit/sensors/MainActivity.java
@@ -63,3 +63,58 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 }
 
+//public class MainActivity extends AppCompatActivity implements SensorEventListener{
+//
+//    public static final String TAG = "MA";
+//    SensorManager sensorManager;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+//
+//        List<Sensor> mySensors =sensorManager.getSensorList(Sensor.TYPE_ALL);
+//
+//        for (Sensor sensor : mySensors){
+//            Log.d(TAG,"onCreate : name = " + sensor.getName());
+//            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT_WATCH) {
+//                Log.d(TAG, "onCreate : type = " + sensor.getStringType());
+//            }
+//            Log.d(TAG,"onCreate : vendor = " + sensor.getVendor());
+//            Log.d(TAG,"onCreate : version = " + sensor.getVersion());
+//            Log.d(TAG,"onCreate : ==============================");
+//        }
+//
+//        final Sensor accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//
+//        Sensor lightsensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+//        SensorEventListener sensorEventListener = new SensorEventListener() {
+//
+//            @Override
+//            public void onSensorChanged(SensorEvent event) {
+//                Log.d(TAG,"onSensorChanged: ax "+event.values[0]);
+//                Log.d(TAG,"onSensorChanged: ay "+event.values[1]);
+//                Log.d(TAG,"onSensorChanged: az "+event.values[2]);
+//            }
+//
+//            @Override
+//            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//            }
+//        };
+//
+//        sensorManager.registerListener(sensorEventListener,accelSensor,1000*1000);
+//        sensorManager.registerListener(sensorEventListener,lightsensor,10*1000);
+//    }
+//
+//    @Override
+//    public void onSensorChanged(SensorEvent event) {
+//
+//    }
+//
+//    @Override
+//    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//
+//    }
+//}
